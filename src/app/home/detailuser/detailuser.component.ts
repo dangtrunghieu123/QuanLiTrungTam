9855954706c57c594227a1a2f309a37bf8818b74
thu le <dangtrunghieu123@gmail.com>
2018-11-17 16:11:14 +0700

    the thirt time

--- src/app/home/detailuser/detailuser.component.ts
+++ src/app/home/detailuser/detailuser.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Course } from '../../model/course';
+import { User } from '../../Model/user';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-detailuser',
   templateUrl: './detailuser.component.html',
   styleUrls: ['./detailuser.component.css']
 })
-export class DetailuserComponent implements OnInit {
+export class DetailuserComponent implements OnInit, AfterViewInit {
+  public listUser: User[] = [];
+  public IDUser: string;
+  public DetailUser: any;
+  public status: boolean = true;
+  public listCourse: Course[] = [];
+  constructor(private activate: ActivatedRoute, private userSV: UserService) { }
+  GetListUser() {
+    this.activate.params.subscribe(
+      (R_detail) => {
+        this.IDUser = R_detail.idUser;
+        console.log(this.IDUser);
+        this.userSV.getInforUser(this.IDUser).subscribe(
+          (result) => {
+            console.log(result);
+            for (let i in result) {
+              this.DetailUser = result[0];
+            }
+            console.log(this.DetailUser.HoTen);
+          },
+          (error) => {
+            console.log(error);
+          }
+        )
+        // this.userSV.getInfoCourse(this.IDUser).subscribe(
+        //   (C_result) => {
+        //     console.log(C_result);
+        //     if(this.status = false){
+        //       alert(C_result);
+        //     }
 
-  constructor() { }
+        //   },
+        //   (C_error) => {
+        //     console.log(C_error);
 
-  ngOnInit() {
+        //   }
+        // )
+
+
+      },
+      (E_detail) => {
+        console.log(E_detail);
+      }
+    )
+
   }
 
+  show() {
+    this.status = true;
+  }
+  hide() {
+    this.status = false;
+  }
+  ngOnInit() {
+    this.GetListUser();
+  }
+
+  ngAfterViewInit() {
+
+  }
 }
